6b8a79b5c778b5dfcc8b7f4b3070d6e3692dd48f
Kay Wu <wuyingying.kay@gmail.com>
2015-01-05 15:07:36 +0800

    增加后台自动更新天气的功能，修改程序图标和名称

--- src/com/coolweather/app/activity/WeatherActivity.java
+++ src/com/coolweather/app/activity/WeatherActivity.java
@@ -14,6 +14,7 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.coolweather.app.R;
+import com.coolweather.app.service.AutoUpdateService;
 import com.coolweather.app.util.HttpCallbackListener;
 import com.coolweather.app.util.HttpUtil;
 import com.coolweather.app.util.Utility;
@@ -137,6 +138,8 @@ public class WeatherActivity extends Activity implements OnClickListener {
 		tvCurrentDate.setText(prefs.getString("current_date", ""));
 		layoutWeatherInfo.setVisibility(View.VISIBLE);
 		tvCity.setVisibility(View.VISIBLE);
+		Intent intent = new Intent(this, AutoUpdateService.class);
+		startService(intent);
 	}
 
 }
---
+++ src/com/coolweather/app/service/AutoUpdateService.java
@@ -0,0 +1,59 @@
+package com.coolweather.app.service;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.IBinder;
+import android.os.SystemClock;
+import android.preference.PreferenceManager;
+
+import com.coolweather.app.util.HttpCallbackListener;
+import com.coolweather.app.util.HttpUtil;
+import com.coolweather.app.util.Utility;
+
+public class AutoUpdateService extends Service {
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		return null;
+	}
+	
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+		new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				updateWeather();
+			}
+		}).start();
+		
+		AlarmManager manager = (AlarmManager) getSystemService(ALARM_SERVICE);
+		int updateInterval = 8 * 60 * 60 * 1000; // 这是8小时的毫秒数
+		long triggerAtTime = SystemClock.elapsedRealtime() + updateInterval;
+		Intent i = new Intent(this, AutoUpdateService.class);
+		PendingIntent pi = PendingIntent.getBroadcast(this, 0, i, 0);
+		manager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, triggerAtTime, pi);
+		return super.onStartCommand(intent, flags, startId);
+	}
+
+	private void updateWeather() {
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+		String countyCode = prefs.getString("county_code", "");
+		String address = "http://www.weather.com.cn/data/cityinfo/" + countyCode + ".html";
+		HttpUtil.sendHttpRequest(address, new HttpCallbackListener() {
+			
+			@Override
+			public void onFinish(String response) {
+				Utility.handleWeatherResponse(AutoUpdateService.this, response);
+			}
+			
+			@Override
+			public void onError(Exception e) {
+				e.printStackTrace();
+			}
+		});
+	}
+}
